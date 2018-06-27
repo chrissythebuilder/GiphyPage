7e36087a83206e917e2361172e666ff54008c222
chrissydc <bychriscelle@gmail.com>
2018-06-26 23:57:37 -0400

    right giphy images show up for each element in array

--- javascript/app.js
+++ javascript/app.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     for (i=0;i<topics.length;i++) {
         var b = $("<button>");
         b.addClass("themes");
-        b.attr("data-emotion");
+        b.attr("data-emotion", topics[i]);
         var text = b.text(topics[i]);
         $("#theme_buttons").append(text);
     }
@@ -13,7 +13,7 @@ $(document).ready(function () {
     $("#submit").on("click", function() {
         var newButton = $("<button>");
         newButton.addClass("themes");
-        newButton.attr("data-emotion")
+        newButton.attr("data-emotion", topics[i])
         var newInput = $("#new").val().trim();
         var newGiphy = newButton.text(newInput);
         $("#theme_buttons").append(newGiphy);
@@ -26,16 +26,14 @@ $(document).ready(function () {
     
 
     $("button").on("click", function () {
-        // var emotions = $(this).attr("data-emotion");
-
-
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&api_key=zipR5lDP7zHvQbilHk9F5jxBOCyLbmMG&limit=10";  
+        var emotion = $(this).attr("data-emotion");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emotion + "&rating=pg&api_key=zipR5lDP7zHvQbilHk9F5jxBOCyLbmMG&limit=10";  
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }) .then (function (response) {
-            for (i=0;i<topics.length;i++) {
+            for (i=0;i<response.data.length;i++) {
                 if (response.data[i].rating !== "r" && response.data[i].rating !== "pg-13") {
                     var gifSection = $("<div class='images'>");
                     var p = $("<p>").html("Rating: " + "<strong>" + response.data[i].rating + "</strong>");
@@ -46,11 +44,12 @@ $(document).ready(function () {
                     gifSection.append(p);
                     gifSection.append(img);
                     $("#giphyImages").prepend(gifSection);
-                }
+                };
             };
-            $(".giphys").on("click", function () {
+            $("img").on("click", function () {
                 var img = $("<img>");
                 img.attr("src", response.data[i].images.original.url);
+                console.log(response);
             })
         })
 
@@ -66,8 +65,9 @@ $(document).ready(function () {
 // QUESTIONS:
 // ratings are undefined (X)
 // images are undefined (X)
-// topics - how to loop for each emotion gif to be generated
+// topics - how to loop for each emotion gif to be generated (x)
+// appending all images - how to just keep for the specific emotion
 // on click for motion
 // added values in the array does not generate images
-// setting attributes to each button
-// topics.length vs 11 in for loop
+// setting attributes to each button (x)
+// topics.length vs 11 in for loop (x)
